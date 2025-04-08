195272761f03ca1e498ee4ff9d162737eb4430ea
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2025-04-08 15:35:54 +0200

    Add homepage and default layout

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app>
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="text-center p-8">
+            <h1 class="text-3xl">{{ config('app.name') }}</h1>
+            <p>{{ config('app.version') }}</p>
+        </div>
+    </div>
+</x-layouts.app>
