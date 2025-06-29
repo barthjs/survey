ff1f2235eaa9cb960da19651ffbf96117c379163
Jonas Barth <143815039+barthjs@users.noreply.github.com>
2025-06-29 15:29:48 +0200

    Add favicon

--- resources/views/components/layouts/html.blade.php
+++ resources/views/components/layouts/html.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
     @vite('resources/css/app.css')
 </head>
 
